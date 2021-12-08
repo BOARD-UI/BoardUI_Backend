36043dceabfd8c08edf4b9f7e3ff82ea798870a2
Luis Amaya <luis.amaya-o@mail.escuelaing.edu.co>
2021-12-08 14:51:05 -0500

    Test Azure

--- src/main/java/edu/escuelaing/arsw/boardUIApi/BoardUIApplication.java
+++ src/main/java/edu/escuelaing/arsw/boardUIApi/BoardUIApplication.java
@@ -5,12 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "edu.escuelaing.arsw.boardUI" })
 @EntityScan(basePackages = { "edu.escuelaing.arsw.boardUI" })
 @EnableJpaRepositories(basePackages = { "edu.escuelaing.arsw.boardUI" })
 public class BoardUIApplication {
+
+	@GetMapping("/message")
+	public String message() {
+		return "Congrats, Application Deployed Successfully";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(BoardUIApplication.class, args);
 	}
